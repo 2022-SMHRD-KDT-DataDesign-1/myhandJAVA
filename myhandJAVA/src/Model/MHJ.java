9997aac08aef5f3b975ba6953f1d862a3814de52
SMHRD <SMHRD@211.33.239.161>
2023-01-20 12:46:35 +0900

    2023-01-20/신석우

--- myhandJAVA/src/Model/MHJ.java
+++ myhandJAVA/src/Model/MHJ.java
@@ -1,6 +1,6 @@
 package Model;
 
-public class dkdkd {
+public class MHJ {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
--- myhandJAVA/src/View/LoginPage.java
+++ myhandJAVA/src/View/LoginPage.java
@@ -1,5 +1,8 @@
 package View;
 
 public class LoginPage {
-
+	
+	public static void main(String[] args) {
+		System.out.println("내손을JAVA 화이팅");
+	}
 }
